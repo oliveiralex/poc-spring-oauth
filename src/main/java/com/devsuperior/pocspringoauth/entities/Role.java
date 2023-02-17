34bc101cd1a36b83404fbca4a6e619a5246d7a3b
oliveiralex <alexandre.si.ufu@gmail.com>
2023-02-17 11:31:12 -0300

    Create User and Role entities, h2 config

---
+++ src/main/java/com/devsuperior/pocspringoauth/entities/Role.java
@@ -0,0 +1,34 @@
+package com.devsuperior.pocspringoauth.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_role")
+public class Role {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String authority;
+	
+	public Role() {
+	}
+
+	public Role(Long id, String authority) {
+		super();
+		this.id = id;
+		this.authority = authority;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+}
