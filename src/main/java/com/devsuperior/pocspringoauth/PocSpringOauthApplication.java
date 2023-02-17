85699f7611cd622b0bf1582774aa85f0b3812d0a
oliveiralex <alexandre.si.ufu@gmail.com>
2023-02-17 11:21:48 -0300

    Project created

---
+++ src/main/java/com/devsuperior/pocspringoauth/PocSpringOauthApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.pocspringoauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocSpringOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocSpringOauthApplication.class, args);
+	}
+
+}
