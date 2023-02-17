942e0892b13ca3c8d4e3cbb4f7419a0b119291cc
oliveiralex <alexandre.si.ufu@gmail.com>
2023-02-17 14:04:58 -0300

    Add some dependecies, extract all claims from token JwtService

--- src/main/java/com/devsuperior/pocspringoauth/services/JwtService.java
+++ src/main/java/com/devsuperior/pocspringoauth/services/JwtService.java
@@ -1,8 +1,36 @@
 package com.devsuperior.pocspringoauth.services;
 
+import java.security.Key;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 
 @Service
 public class JwtService {
+	
+	@Value("${jwt.secret}")
+	private String jwtSecret;
 
+	public String extractUsername(String token) {
+		return null;
+	}
+
+	private Claims extractAllClaims(String token) {
+		return Jwts
+				.parserBuilder()
+				.setSigningKey(getSignInKey())
+				.build()
+				.parseClaimsJws(token)
+				.getBody();
+	}
+
+	private Key getSignInKey() {
+		byte[] keyBytes = Decoders.BASE64.decode(jwtSecret);
+		return Keys.hmacShaKeyFor(keyBytes);
+	}
 }
